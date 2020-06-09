aa070878531b5f898cd238a7f6c46ba545346de1
Dima Parzhitsky <59528510+parzhitsky@users.noreply.github.com>
2020-06-09 19:03:58 +0300

    Define add()

--- calculator/add.js
+++ calculator/add.js
@@ -1,1 +1,20 @@
-module.exports = null;
+/** @public */
+const DEFAULT_SUMMAND = 0;
+
+/**
+ * @public
+ * @param {number} x
+ * @param {number} y
+ * @returns {number}
+ */
+function add(x = DEFAULT_SUMMAND, y = DEFAULT_SUMMAND) {
+  return Number(x) + Number(y);
+}
+
+module.exports = {
+  // the primary entity
+  add,
+
+  // the secondary entities (only one in this module)
+  DEFAULT_SUMMAND,
+};
