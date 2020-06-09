9500e4cc4b9c414217ee214384b0bb5a8d26f52a
Dima Parzhitsky <59528510+parzhitsky@users.noreply.github.com>
2020-06-09 19:06:41 +0300

    Define subtract()

--- calculator/subtract.js
+++ calculator/subtract.js
@@ -1,1 +1,18 @@
-module.exports = null;
+const { add, DEFAULT_SUMMAND } = require("./add");
+
+/**
+ * @public
+ * @param {number} x
+ * @param {number} y
+ * @returns {number}
+ */
+function subtract(x, y = DEFAULT_SUMMAND) {
+  return add(x, -y);
+}
+
+module.exports = {
+  // the primary entity
+  subtract,
+
+  // the secondary entities (none in this module)
+};
