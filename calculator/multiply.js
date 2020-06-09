bb1a0c2164e5a6562010fc31de97635bb9894b00
Dima Parzhitsky <59528510+parzhitsky@users.noreply.github.com>
2020-06-09 19:09:22 +0300

    Define multiply()

--- calculator/multiply.js
+++ calculator/multiply.js
@@ -1,1 +1,20 @@
-module.exports = null;
+/** @public */
+const DEFAULT_FACTOR = 1;
+
+/**
+ * @public
+ * @param {number} x
+ * @param {number} y
+ * @returns {number}
+ */
+function multiply(x = DEFAULT_FACTOR, y = DEFAULT_FACTOR) {
+  return Number(x) * Number(y);
+}
+
+module.exports = {
+  // the primary entity
+  multiply,
+
+  // the secondary entities (only one in this module)
+  DEFAULT_FACTOR,
+};
