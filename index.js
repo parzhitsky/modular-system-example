ee44d84886a27e8e85d521e15d02ca4f1082a32a
Dima Parzhitsky <59528510+parzhitsky@users.noreply.github.com>
2020-06-09 19:55:49 +0300

    Define entry

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-module.exports = null;
+const parseExpression = require("./parse-expression");
+
+/*
+ * Usage in CLI:
+ * node index.js 1 + 2
+ * node index.js 3 - 4
+ * node index.js 5 * 6
+ * node index.js 7 / 8
+ */
+
+const { expression, x, operation, y } = parseExpression(...process.argv.slice(2));
+
+if (operation == null) {
+  console.error(`Unknown operation: "${ expression }"`);
+  process.exit(1);
+}
+
+console.log(`${ expression } = ${ operation(x, y) }`);
