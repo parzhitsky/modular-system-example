2dec26701b0678d9b0eec6cfc68c2038de5ba730
Dima Parzhitsky <59528510+parzhitsky@users.noreply.github.com>
2020-06-09 19:10:31 +0300

    Define multiply()

--- calculator/divide.js
+++ calculator/divide.js
@@ -1,1 +1,18 @@
-module.exports = null;
+const { multiply, DEFAULT_FACTOR } = require("./multiply");
+
+/**
+ * @public
+ * @param {number} x
+ * @param {number} y
+ * @returns {number}
+ */
+function divide(x, y = DEFAULT_FACTOR) {
+  return multiply(x, 1 / y);
+}
+
+module.exports = {
+  // the primary entity
+  divide,
+
+  // the secondary entities (none in this module)
+};
